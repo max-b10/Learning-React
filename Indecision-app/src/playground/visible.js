d859c2e8bf321cfcf1bb940ccae9a569731d26ff
max-b10 <bungaymax@gmail.com>
2021-02-24 13:21:09 +0000

    Practice

---
+++ Indecision-app/src/playground/visible.js
@@ -0,0 +1,25 @@
+let visibility = false;
+
+const toggleVisibility = () => {
+  visibility = !visibility;
+  render();
+};
+
+const render = () => {
+  const jsx = (
+    <div>
+      <h1>Visibility Toggle</h1>
+      <button onClick={toggleVisibility}>
+        {visibility ? "Hide Details" : "Show Details"}
+      </button>
+      {visibility && (
+        <div>
+          <p>Some details you can now see</p>
+        </div>
+      )}
+    </div>
+  );
+
+  ReactDOM.render(jsx, document.getElementById("app"));
+};
+render();
