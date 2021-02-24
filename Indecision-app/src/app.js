c314d27f386346f98919aee908a7e3cde97b90ec
max-b10 <bungaymax@gmail.com>
2021-02-24 12:23:08 +0000

    Updates

--- Indecision-app/src/app.js
+++ Indecision-app/src/app.js
@@ -1,44 +1,54 @@
-console.log("app.js is running");
-// JSX - JavaScript XML
+console.log("App.js is running!");
+// App Object:
 const app = {
-  title: "This is the title",
-  subtitle: "This is the subtitle",
-  options: ["one", "two"],
+  title: "Indecision App",
+  subtitle: "Put your life in the hands of a computer",
+  options: [],
 };
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? "Here are your options:" : "no options"}</p>
-    <ol>
-      <li>Option 1</li>
-      <li>Option 2</li>
-    </ol>
-  </div>
-);
-
+// Functions:
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    render();
+  }
+};
+const removeAll = () => {
+  app.options = [];
+  render();
+};
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  console.log(randomNum, option);
+};
+// The <h1> id='app' from index.html:
 const appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
 
-// const user = {
-//   //   name: "Jim",
-//   age: 18,
-//   location: "London",
-// };
-
-// function getLocation(location) {
-//   if (location) {
-//     return <p>Location: {location}</p>;
-//   }
-// }
-// const templateTwo = (
-//   <div>
-//     <h1>Name: {user.name ? user.name : "Anonymous"}</h1>
-//     {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
-//     {getLocation(user.location)}
-//   </div>
-// );
-// const appRoot = document.getElementById("app");
-
-// ReactDOM.render(templateTwo, appRoot);
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>
+        What should i do?
+      </button>
+      <button onClick={removeAll}>Remove all</button>
+      <ol>
+        {app.options.map((option) => (
+          <li key={option}>{option}</li>
+        ))}
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" placeholder="Type option here"></input>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, appRoot);
+};
+render();
---
+++ Indecision-app/src/playground/arrow-func.js
@@ -0,0 +1,9 @@
+const square = (a) => a * a;
+console.log(square(5));
+
+const getFirstName = (fullName) => fullName.split(" ")[0];
+console.log(getFirstName("Bob Smith"));
+
+// Or with 2 arguments:
+// const getFirstName = (firstName, lastName) => firstName;
+// console.log(getFirstName("Bob", "Smith"));
